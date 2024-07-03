e7141be1f2d96823e3f7d32f6021ec995485d5e7
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-07-03 17:34:54 -0300

    Configurando primeiros detalhes do front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
-import './App.css'
+import GlobalStyle from './styles/global';
+import styled from "styled-components";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
+
 
 function App() {
 
   return (
     <>
-
+      <Container>
+        <Title> Usuários </Title>
+      </Container>
+      <ToastContainer autoClose={3000} position="bottom-left"/>
+      <GlobalStyle/>
     </>
   )
 }
