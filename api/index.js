8e0aa8b4a003364ac6b87f063751bd624a59763c
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-07-03 15:57:19 -0300

    Configurando api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+import userRoutes from './routes/users.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(8000)
+app.use("/", userRoutes)
+
+app.listen(8800)
